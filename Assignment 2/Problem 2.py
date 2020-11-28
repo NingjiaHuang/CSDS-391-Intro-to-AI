b1992459ee3a5868878d006f18cc5c93e60eb116
Ningjia Huang <nxh239@case.edu>
2020-11-27 20:53:27 -0500

    Added problem 2 b.

--- Assignment 2/Problem 2.py
+++ Assignment 2/Problem 2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
 import math
-from sklearn import linear_model
 
 def read_data():
     df = pd.read_csv('irisdata.csv')
@@ -9,13 +9,15 @@ def read_data():
 
 def one_layer_network(w0, w1, w2, petal_length, petal_width):
     h_xj = w0 + w1 * petal_length + w2 * petal_width
-    logistic = 1 / (1 + math.exp(-h_xj)) 
-    return logistic
+    sigmoid = 1 / (1 + math.exp(-h_xj))
+    if (sigmoid < 0.5):
+	    return 0
+    else:
+	    return 1
 
-# data vectors in dataframe, pattern classes in list
+# data vectors in dataframe, pattern classes in dataframe
 def mean_square_error(data_vectors, w0, w1, w2, pattern_classes):
     n = data_vectors.shape[0]
-    print("# of rows: ", n)
     data_vectors_list = data_vectors.values.tolist()
     pattern_classes_list = pattern_classes.tolist()
     temp_mse = 0
@@ -33,10 +35,25 @@ def iris_customize():
     y.replace(['versicolor','virginica'], [0,1], inplace=True)
     return x, y
 
+def plot(x, y):
+    df = read_data()
+    versicolor = df[df['species']=='versicolor']
+    virginica = df[df['species']=='virginica']
+    plt.plot(versicolor["petal_length"], versicolor["petal_width"], '*',label="versicolor", color = 'orange')
+    plt.plot(virginica["petal_length"], virginica["petal_width"], '+',label="virginica", color = 'blue')
+    plt.xlabel("petal length (cm)")
+    plt.ylabel("petal width (cm)")
+    plt.plot(x,y)
+    plt.legend()
+    plt.show()
+
 def main():
     x, y = iris_customize()
-    mse = mean_square_error(x, -2.8, 0.25, 1, y)
-    print(mse)
+    w0 = -1
+    w1 = 0.7
+    w2 = 2
+    mse = mean_square_error(x, w0, w1, w2, y)
+    plot([0,-w0/w1],[-w0/w2,0])
 
 if __name__ == "__main__":
     main()
