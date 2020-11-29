bef558064202b39b158f4d1326fe39b91b37f180
sherryz06 <72478830+sherryz06@users.noreply.github.com>
2020-11-29 15:03:34 -0500

    Update problem1.py
    Separated sigmoid and output

--- Assignment 2/problem1.py
+++ Assignment 2/problem1.py
@@ -7,6 +7,8 @@ import numpy as np
 import sklearn.linear_model
 from sklearn.linear_model import LogisticRegression 
 from mpl_toolkits.mplot3d import *
+import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 
 #load iris dataset
 data = pd.read_csv("/Users/tianxi/Desktop/irisdata.csv")
@@ -36,7 +38,7 @@ def plotclasses23() -> None:
 	plt.show()
 
 #function that computes the output of simple one-layer neural network
-def output(species):
+def zValue(species):
     n = 1
     y = []
     for var in species:
@@ -48,13 +50,16 @@ def output(species):
             print("error\n")
     return y
 
+def sigmoid(length:float, width: float):
+	w = [-3.18, 0.30, 0.98]
+	z = w[0] + w[1] * length + w[2] * width
+	sigmoid = 1 / (1 + math.exp(-z))
+	return sigmoid
 
 #Exercise 1. b. computes the output of simple one-layer neural network
-def logistic(length:float, width: float) -> float: 
-	w = [-3.18, 0.30, 0.98]
-	z = (w[0] + (w[1]*length) + (w[2]*width))
-	sigmoid = 1 / (1 + math.exp(-z))
-	if (sigmoid < 0.5):
+def output(length:float, width: float) -> float: 
+	sig = sigmoid(length, width)
+	if (sig < 0.5):
 		return 0
 	else:
 		return 1
@@ -83,13 +88,13 @@ def plotNeuralNetwork(dataset: list) -> None:
 	x = np.arange(3.0, 7.0, 0.01) 
 	y = np.arange(1.0, 2.5, 0.01) 
 	X, Y = np.meshgrid(x, y)
-	z = np.array([logistic(a,b) for a,b in zip(np.ravel(X), np.ravel(Y))])
+	z = np.array([output(a,b) for a,b in zip(np.ravel(X), np.ravel(Y))])
 	Z = z.reshape(X.shape)
 
-	z2 = output(versicolor['species'])
-	z3 = output(virginica['species'])
+	z2 = zValue(versicolor['species'])
+	z3 = zValue(virginica['species'])
 
-	ax.plot_surface(X, Y, Z)
+	ax.plot_surface(X, Y, Z, cstride=1, rstride=1, linewidth=0, antialiased=True, color = 'grey')
 	ax.scatter3D(versicolor["petal_length"], versicolor['petal_width'], z2, marker = '*', color = 'orange') 
 	ax.scatter3D(virginica["petal_length"], virginica['petal_width'], z3, marker = '+', color = 'blue') 
 	plt.title("Output of neural network")
@@ -102,8 +107,8 @@ def plotNeuralNetwork(dataset: list) -> None:
 def main() -> None:
 	class23 = data[data['species'] != 'setosa']
 	#print(class23)
-	plotclasses23()
-	decisionBoundary(class23)
+	#plotclasses23()
+	#decisionBoundary(class23)
 	plotNeuralNetwork(class23)
 
 main()
