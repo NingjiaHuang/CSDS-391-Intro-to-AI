7ba1083b69b02797de5f7e2a78da81b5f1bf4a20
sherryz06 <72478830+sherryz06@users.noreply.github.com>
2020-11-29 16:49:35 -0500

    Update problem1.py
    Added Exercise 1.e

--- Assignment 2/problem1.py
+++ Assignment 2/problem1.py
@@ -9,10 +9,10 @@ from sklearn.linear_model import LogisticRegression
 from mpl_toolkits.mplot3d import *
 import matplotlib.cm as cm
 from matplotlib.colors import Normalize
+from matplotlib.lines import Line2D
 
 #load iris dataset
 data = pd.read_csv("/Users/tianxi/Desktop/irisdata.csv")
-#print(data.head())
 
 setosa = data[data['species']=='setosa']
 versicolor = data[data['species']=='versicolor']
@@ -33,11 +33,11 @@ def plotclasses23() -> None:
 	plt.plot(virginica["petal_length"], virginica["petal_width"], '+',label="virginica", color = 'blue')
 	plt.xlabel("petal length (cm)")
 	plt.ylabel("petal width (cm)")
-	plt.title("the 2nd and 3rd iris classes")
+	plt.title("The 2nd and 3rd Iris Classes")
 	plt.legend()
 	plt.show()
 
-#function that computes the output of simple one-layer neural network
+#Output of 0 for the 2nd iris class, and 1 for the 3rd.
 def zValue(species):
     n = 1
     y = []
@@ -50,7 +50,8 @@ def zValue(species):
             print("error\n")
     return y
 
-def sigmoid(length:float, width: float):
+#sigmoid function
+def sigmoid(length:float, width: float) -> float:
 	w = [-3.18, 0.30, 0.98]
 	z = w[0] + w[1] * length + w[2] * width
 	sigmoid = 1 / (1 + math.exp(-z))
@@ -75,10 +76,10 @@ def decisionBoundary(dataset: list) -> None:
     
 	plt.plot(x, y, 'g-')
 
-	plt.title("decision boundary")
+	plt.title("Decision Boundary")
 	plt.xlabel('Petal Length (cm)')
 	plt.ylabel('Petal Width (cm)')
-
+	plt.legend()
 	plt.show()
 
 #Exercise 1. d. plot the output of neural network
@@ -97,18 +98,59 @@ def plotNeuralNetwork(dataset: list) -> None:
 	ax.plot_surface(X, Y, Z, cstride=1, rstride=1, linewidth=0, antialiased=True, color = 'grey')
 	ax.scatter3D(versicolor["petal_length"], versicolor['petal_width'], z2, marker = '*', color = 'orange') 
 	ax.scatter3D(virginica["petal_length"], virginica['petal_width'], z3, marker = '+', color = 'blue') 
-	plt.title("Output of neural network")
+	plt.title("Output of Neural Network")
 	ax.set_xlabel('Petal length (cm)')
 	ax.set_ylabel('Petal width (cm)')
 	ax.set_zlabel('output')
 	plt.show()
 
+#Exercise 1. e. show the output of your simple classifier using examples
+def examples(dataset: list) -> None:
+	# 4.8 1.8 versicolor
+	# 5.0 1.7 versicolor
+	# 3.5 1.0 versicolor
+	# 3.8 1.1 versicolor
+	versicolorLength = [4.8, 5.0, 3.5, 3.8]
+	versicolorWidth = [1.8, 1.7, 1.0, 1.1]
+	z = []
+	for var1, var2 in zip(versicolorLength, versicolorWidth):
+		z.append(output(var1, var2))
+	for var1, var2, var3 in zip(versicolorLength, versicolorWidth, z):
+		if (var3 == 0):
+			plt.plot(var1, var2, '*',label="versicolor, classified as versicolor", color = 'orange')
+		else:
+			plt.plot(var1, var2, '*',label="versicolor, classified as virginica", color = 'blue')
+	
+	# 5.1 1.8 virginica
+	# 5.0 1.5 virginica
+	# 4.9 1.8 virginica
+	# 5.9 2.3 virginica
+	virginicaLength = [5.1, 5.0, 4.9, 5.9]
+	virginicaWidth = [1.8, 1.5, 1.8, 2.3]
+	z = []
+	for var1, var2 in zip(virginicaLength, virginicaWidth):
+		z.append(output(var1, var2))
+	for var1, var2, var3 in zip(virginicaLength, virginicaWidth, z):
+		if (var3 == 0):
+			plt.plot(var1, var2, '+',label="virginica, classified as versicolor", color = 'orange')
+		else:
+			plt.plot(var1, var2, '+',label="virginica, classified as virginica", color = 'blue')
+
+	w = [-3.18, 0.30, 0.98]
+	x = dataset['petal_length']
+	y = [-(w[1] * x_value + w[0]/ w[2]) for x_value in x] 
+	plt.plot(x, y, 'g-')
+
+	plt.title("Examples")
+	plt.xlabel('Petal Length (cm)')
+	plt.ylabel('Petal Width (cm)')
+	plt.show()
 
 def main() -> None:
 	class23 = data[data['species'] != 'setosa']
-	#print(class23)
-	#plotclasses23()
-	#decisionBoundary(class23)
+	plotclasses23()
+	decisionBoundary(class23)
 	plotNeuralNetwork(class23)
+	examples(class23)
 
 main()
